31691f023c691ae469cc32a534ff1328b36235d8
VillamilA <angel.villamil@epn.edu.ec>
2025-12-02 22:57:49 -0500

    correcion del protocolo

--- lib/src/data/services/auth_service.dart
+++ lib/src/data/services/auth_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AuthService {
   AuthService._();
@@ -11,6 +12,23 @@ class AuthService {
       Uri.parse(url),
       body: {'email': email, 'password': password},
     );
-    // Procesa la respuesta y retorna los datos
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Error en el login: ${response.body}');
+    }
+  }
+
+  Future<dynamic> register(Map<String, dynamic> data) async {
+    final url = '${dotenv.env['API_BASE_URL']}${dotenv.env['API_REGISTER_PATH']}';
+    final response = await http.post(
+      Uri.parse(url),
+      body: data.map((k, v) => MapEntry(k, v.toString())),
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Error en el registro: ${response.body}');
+    }
   }
 }
